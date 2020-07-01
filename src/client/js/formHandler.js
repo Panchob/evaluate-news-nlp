964a2f7f9fb239a34655c1bbf587c543a44faa17
Francis <bilodeaurobillardfrancis@gmail.com>
2020-07-01 08:22:58 -0400

    Feat: new endpoints and how to handle them

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,43 @@
-
 function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let url = document.getElementById('name').value
-    
-    console.log("::: Form Submitted :::")
+    const html_results =  document.getElementById('results')
+
+
     if(validURL(url))
     {
-        generateSummary('http://localhost:8000/summary', {article:url})
-        .then(function(newData) {
-            const text = document.createTextNode(newData.summary);
-            document.getElementById('results').innerHTML = ''
-            document.getElementById('results').appendChild(text);
-        });
+        let post_title = title('http://localhost:8000/extract', {article:url});
+        let post_summary = summarize('http://localhost:8000/summary', {article:url});
+        let post_sentiment = sentiment('http://localhost:8000/sentiment', {article:url})
+
+        Promise.all([post_title, 
+                     post_summary, 
+                     post_sentiment
+            ]).then(function(responses) {
+                return Promise.all(responses.map(function(response){
+                    return response;
+                    }));
+            }).then(function(data){
+
+                let html_result = `
+                                   <div id="summary">
+                                     <h3 id="articleTitle">${data[0].title}</h3>
+                                     <p>${data[1].summary}</p>
+                                  </div>
+                                  <div id="sentiment">
+                                     <p id="polarity"><strong>Polarity: </strong>${data[2].polarity}</p>
+                                     <p id="subjectivity"><strong>Subjectivity: </strong>${data[2].subjectivity}</p>
+                                   </div>`;
+
+                html_results.innerHTML = '';
+                html_results.insertAdjacentHTML("afterbegin", html_result);
+
+            }).catch(function(error){
+                console.log(error)
+            });
+
     }
     else
     {
@@ -22,7 +46,7 @@ function handleSubmit(event) {
 
 }
 
-const generateSummary = async(url='', data = {})=>{
+const summarize = async(url='', data = {})=>{
     const res = await fetch(url, {
         method: 'POST',
         credentials: 'same-origin',
@@ -33,10 +57,46 @@ const generateSummary = async(url='', data = {})=>{
     });
 
     try{
-        const newData = await res.json();
-        return newData
+        const summary = await res.json();
+        return summary;
     }catch(error){
-        console.log("error", error)
+        console.log("error", error);
+    }
+};
+
+const title = async(url='', data = {})=>{
+    const res = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    try{
+        const title = await res.json();
+        return title;
+    }catch(error){
+        console.log("error", error);
+    }
+};
+
+const sentiment = async(url='', data = {})=>{
+    const res = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    try{
+        const sentiment = await res.json();
+        return sentiment;
+    }catch(error){
+        console.log("error", error);
     }
 };
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,4 @@
-var path = require('path')
+var path = require('path');
 var aylien = require("aylien_textapi");
 
 const bodyParser = require('body-parser');
@@ -9,16 +9,17 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 
 console.log(__dirname)
 
-// set aylien API credentias
+
+// set aylien API credentiaLs
 var textapi = new aylien({
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY
@@ -29,29 +30,52 @@ app.get('/', function (req, res) {
     res.sendFile(path.resolve('src/client/views/index.html'));
 });
 
-app.post('/summary', summarize);
+app.post('/summary', function summarize(req, res){
+    article = req.body.article
+    summary = []
 
-function summarize(req, res)
-{
-    let summary = []
-    const data = req.body
-    console.log(data)
     textapi.summarize({
-        url: data.article,
+        url: article,
         sentences_number: 3
         }, function(error, response) {
             if (error === null) {
                 response.sentences.forEach(function(s) {
                     summary.push(s);
-
                 });
-                console.log(summary)
-                res.send({'summary':summary})
+                res.send({'summary':summary});
             }
         });
-}
+});
+
+app.post('/extract', function extract(req, res){
+    article = req.body.article
+
+    textapi.extract({
+        url: article,
+    }, function(error, response) {
+        if (error === null) {
+            res.send({'title':response.title})
+        }
+    });
+});
+    
+
+app.post('/sentiment', function sentiment(req, res){
+    article = req.body.article
+
+    textapi.sentiment({
+        url: article,
+        mode: 'document'
+      }, function(error, response) {
+        if (error === null) {
+            res.send(response)
+        }
+    });
+});
+
+
 
 // designates what port the app will listen to for incoming requests
 app.listen(8000, function () {
-    console.log('listening on port 8000!')
+    console.log('listening on port 8000!');
 })
