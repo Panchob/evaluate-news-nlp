940e6b240d493066b326f4656f5eac6e25d7e895
Francis <bilodeaurobillardfrancis@gmail.com>
2020-07-01 10:01:49 -0400

    Refactor: extracted url validation

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,3 +1,5 @@
+import { validURL } from './validateURL'
+
 function handleSubmit(event) {
     event.preventDefault()
 
@@ -99,15 +101,5 @@ const sentiment = async(url='', data = {})=>{
         console.log("error", error);
     }
 };
-
-function validURL(myURL) {
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ //port
-    '(\\?[;&amp;a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i');
-    return pattern.test(myURL);
- }
 
 export { handleSubmit }
