0e6b3252a64d0b3abfeb5c1740c5604017ba781a
Francis <bilodeaurobillardfrancis@gmail.com>
2020-07-03 13:52:25 -0400

    Chore: Removed export target

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,7 +8,6 @@ const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
-    target: 'node',
     output: {
         libraryTarget: 'var',
         library: 'Client'
